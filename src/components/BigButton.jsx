c2d430f9a3ef725509631bbc8035ff2e67e801f7
Dragonfire <bastian616-git@yahoo.de>
2017-01-15 20:56:22 +0100

    Fix Big Button onClick

--- src/components/BigButton.jsx
+++ src/components/BigButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class BigButton extends React.Component {
     render() {
-        return <button className={`${this.props.className} btn btn-default btn-lg`}>
+        return <button className={`${this.props.className} btn btn-default btn-lg`} onClick={this.props.onClick}>
             {this.props.children}
         </button>;
     }
