ec47535e6fc260fd1dba1177fd2b8a9f10c949b4
axel12 <alexander.von.trostorff@gmail.com>
2019-09-07 13:59:12 +0200

    Show warning if player not logged in on join
    Copied from SH

--- src/actions/AcceptInvitation.jsx
+++ src/actions/AcceptInvitation.jsx
@@ -9,12 +9,15 @@ import Api from '../utils/Api.jsx';
 import Utils from '../utils/Utils.jsx';
 
 import jwt from 'jwt-simple';
+import Session from '../utils/Session';
 
 export default class AcceptInvitation extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: null
+            data: null,
+            clickedWhenNotLoggedIn: false,
+            joined: false
         };
         this.joinClan = this.joinClan.bind(this);
     }
@@ -22,12 +25,21 @@ export default class AcceptInvitation extends React.Component {
     componentDidMount() {
         let token = location.hash.substring('#token='.length);
         var decoded = jwt.decode(token, null, true);
-        this.setState({ token, data: decoded });
+        this.setState({ token, data: decoded ,clickedWhenNotLoggedIn: false, joined: false});
     }
 
     joinClan() {
+        if (!Session.loggedIn()) {
+            this.state.clickedWhenNotLoggedIn= true;
+            this.setState(this.state);
+            return;
+        }
         Api.post(`/clans/joinClan?token=${this.state.token}`, (response) => {
             console.log(response);
+            if(response.status<=300 && response.status>=200){
+                this.state.joined= true;
+                this.setState(this.state);
+            }
         });
     }
 
@@ -35,11 +47,21 @@ export default class AcceptInvitation extends React.Component {
         if (this.state.data) {
             return <Page title={'Join Clan'}>
                 <div className="well bs-component">
+                    {this.state.clickedWhenNotLoggedIn &&
+                        <div className="alert alert-danger">
+                            <strong>Error!</strong> Please login first and try again...
+                        </div>
+                    }
+                    {this.state.joined &&
+                        <div className="alert alert-success">
+                            <strong>Hurrah!</strong> You joined...
+                        </div>
+                    }
                     <InputPair disabled={true} label="Clan Tag" value={this.state.data.clan.tag} />
                     <InputPair disabled={true} label="Clan Name" value={this.state.data.clan.name} />
                     <InputPair disabled={true} label="Expire At" value={Utils.formatTimestamp(this.state.data.expire)} />
                     <div className="grid" style={{ marginTop: '15px' }}>
-                        <BigButton onClick={this.joinClan} className="col-1-2">Join Clan</BigButton>
+                        <BigButton disabled={this.state.joined} onClick={this.joinClan} className="col-1-2">Join Clan</BigButton>
                         <Link to={`/clan/${this.state.data.clan.id}`} className="col-1-2 btn btn-default btn-lg">Go to Clan Page</Link>
                     </div>
                 </div>
--- src/components/BigButton.jsx
+++ src/components/BigButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class BigButton extends React.Component {
     render() {
-        return <button className={`${this.props.className} btn btn-default btn-lg`} onClick={this.props.onClick}>
+        return <button disabled={this.props.disabled} className={`${this.props.className} btn btn-default btn-lg`} onClick={this.props.onClick}>
             {this.props.children}
         </button>;
     }
